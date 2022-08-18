3c65e899730e15cb9ac92261d823ccd8e907ef5c
GareArc <garethcxy.chen@mail.utoronto.ca>
2022-08-18 10:52:14 -0400

    implement naive representative

--- FOIL/AL.py
+++ FOIL/AL.py
@@ -2,28 +2,32 @@ from modAL.models import ActiveLearner
 from modAL.uncertainty import entropy_sampling
 
 import numpy as np
+import copy
 
 from FoilModel import FoilImageClassifier
 from data import ClassificationDataManager
 from strategies.diversity import diversity_sampling_strategy_global
+from strategies.representativeness import representativeness_query_strategy
 
 class ActiveLearningManager:
-    def __init__(self, learner: ActiveLearner, X_pool, y_pool) -> None:
+    def __init__(self, learner: ActiveLearner, X_pool, y_pool, X_test, y_test) -> None:
         self.learner = learner
         self.X_pool = X_pool
         self.y_pool = y_pool
+        self.X_test = X_test
+        self.y_test = y_test
 
-    def run_active_learning(self, n_queries: int, n_instances: int, plot=True, **query_args) -> None:
+    def run_active_learning(self, n_queries: int, n_instances: int, plot=True, **query_args):
         query_lst = []
         acc_lst = []
         for i in range(n_queries):
-            acc = self.learner.score(X_test, y_test)
+            acc = self.learner.score(self.X_test, self.y_test)
             print(f"Round {i}: Accuracy = {acc}")
             acc_lst.append(acc)
-            print(f"X training set size: {len(learner.X_training)}, X pool size: {len(self.X_pool)}")
-            query_idx, query_item = learner.query(self.X_pool, n_instances=n_instances, **query_args)
+            print(f"X training set size: {len(self.learner.X_training)}, X pool size: {len(self.X_pool)}")
+            query_idx, query_item = self.learner.query(self.X_pool, n_instances=n_instances, **query_args)
             query_lst.append((query_idx, query_item))
-            learner.teach(self.X_pool[query_idx], self.y_pool[query_idx])
+            self.learner.teach(self.X_pool[query_idx], self.y_pool[query_idx])
             self.X_pool = np.delete(self.X_pool, query_idx, axis=0)
             self.y_pool = np.delete(self.y_pool, query_idx, axis=0)
         
@@ -31,11 +35,43 @@ class ActiveLearningManager:
             import matplotlib.pyplot as plt
             with plt.style.context('seaborn-whitegrid'):
                 plt.plot(np.arange(n_queries, dtype=int), acc_lst, label='Accuracy')
-                plt.xlabel(f"Query(+{n_instances} instances each round)")
+                plt.xlabel(f"Query(+{n_instances} instance(s) per round)")
                 plt.ylabel('Accuracy')
                 plt.legend()
                 plt.show()
+        return query_lst, acc_lst
 
+    def run_against_random(self, n_queries: int, n_instances: int, plot=True, **query_args) -> None:
+        # For random
+        random_X_pool = copy.deepcopy(self.X_pool)
+        random_y_pool = copy.deepcopy(self.y_pool)
+        random_leaner = copy.deepcopy(self.learner)
+        random_leaner.query_strategy = random_query_strategy
+        random_query_lst = []
+        random_acc_lst = []
+        for i in range(n_queries):
+            acc = random_leaner.score(self.X_test, self.y_test)
+            print(f"Round {i}: Accuracy = {acc}")
+            random_acc_lst.append(acc)
+            print(f"X training set size: {len(random_leaner.X_training)}, X pool size: {len(random_X_pool)}")
+            query_idx, query_item = random_leaner.query(random_X_pool, n_instances=n_instances, **query_args)
+            random_query_lst.append((query_idx, query_item))
+            random_leaner.teach(random_X_pool[query_idx], random_y_pool[query_idx])
+            random_X_pool = np.delete(random_X_pool, query_idx, axis=0)
+            random_y_pool = np.delete(random_y_pool, query_idx, axis=0)
+
+        # For active learning
+        query_lst, acc_lst = self.run_active_learning(n_queries, n_instances, plot=False, **query_args)
+
+        if plot:
+            import matplotlib.pyplot as plt
+            with plt.style.context('seaborn-whitegrid'):
+                plt.plot(np.arange(n_queries, dtype=int), random_acc_lst, label='Random')
+                plt.plot(np.arange(n_queries, dtype=int), acc_lst, label='Active Learning')
+                plt.xlabel(f"Query(+{n_instances} instance(s) per round)")
+                plt.ylabel('Accuracy')
+                plt.legend()
+                plt.show()
 
 model = FoilImageClassifier()
 
@@ -43,10 +79,10 @@ data_parser = ClassificationDataManager()
 
 X, X_unlabeled, y = data_parser.get_data_from_file()
 
-X_train = np.array(X[:110])
-y_train = np.array(y[:110])
-X_test = np.array(X[-10:])
-y_test = np.array(y[-10:])
+X_train = np.array(X[:90])
+y_train = np.array(y[:90])
+X_test = np.array(X[-30:])
+y_test = np.array(y[-30:])
 initial_idx = np.array([0, 11, 24])
 X_initial = X_train[initial_idx]
 y_initial = y_train[initial_idx]
@@ -55,24 +91,25 @@ y_pool = np.delete(y_train, initial_idx, axis=0)
 
 def random_query_strategy(classifier, X, n_instances=1):
     query_idx = np.random.choice(range(len(X)), size=n_instances, replace=False)
+    print(f"Seleted idx by random: {query_idx}")
     return query_idx, X[query_idx]
 
 def uncertainty_sampling_strategy(classifier, X, n_instances=1):
     probs = np.array(classifier.predict_proba(X))
     print(f"Uncertianty: {probs}")
-    query_idx = np.argsort(probs)[:n_instances]
+    query_idx = np.argsort(probs)[-n_instances:]
     print(f"Seleted to label: {query_idx}")
     return query_idx, X[query_idx]
 
 learner = ActiveLearner(
     estimator=model,
     # query_strategy=uncertainty_sampling_strategy,
-    query_strategy=random_query_strategy,
+    query_strategy=representativeness_query_strategy,
     X_training=X_initial, y_training=y_initial
 )
 
-active_learner = ActiveLearningManager(learner, X_pool, y_pool)
-active_learner.run_active_learning(n_queries=10, n_instances=2)
+active_learner = ActiveLearningManager(learner, X_pool, y_pool, X_test, y_test)
+active_learner.run_against_random(n_queries=50, n_instances=1)
 
 # print(similarity_sample(X_train[2], X_train[1]))
 # print(diversity_sampling_strategy_global(None, X, 5))
--- FOIL/strategies/representativeness.py
+++ FOIL/strategies/representativeness.py
@@ -1,5 +1,6 @@
 from numbers import Number
 import numpy as np
+from modAL.models import ActiveLearner
 import sys
 import os
 
@@ -9,11 +10,32 @@ from FoilModel import FoilImageClassifier
 
 
 
-def representativeness_query_strategy(classifier: FoilImageClassifier, X, n_instances=1):
-    pass
+def representativeness_query_strategy(classifier: ActiveLearner, X, n_instances=1):
+    model: FoilImageClassifier = classifier.estimator
+    X_vec = featurelize(X, model.get_object_list())
+    X_density = compute_density(X_vec)
+    X_density_sorted_idx = np.argsort(X_density)
+    print(f"Selected idx by representative: {X_density_sorted_idx[-n_instances:]}")
+    return X_density_sorted_idx[-n_instances:], X[-n_instances:]
 
 
 # Helpers
+
+def compute_density(X):
+    """
+    Compute the density of the given data.
+    formula: sum(similarity(x1, x2)) / n-1
+    """
+    n = len(X)
+    result = []
+    for i in range(n):
+        density = 0
+        for j in range(n):
+            if i == j:
+                continue
+            density += compute_similarity_measure(X[i], X[j])
+        result.append(density / (n - 1))
+    return np.array(result)
 
 def compute_similarity_measure(x1, x2):
     """
@@ -35,7 +57,10 @@ def featurelize(X, object_lst: list[str]):
     for x in X_objs:
         vec = [0 for _ in range(len(object_lst))]
         for obj in x:
-            vec[object_lst.index(obj)] += 1
+            if obj in object_lst:
+                vec[object_lst.index(obj)] = 1
+            # else:
+            #     print(f"Object not found: {obj}")
         X_vectors.append(vec)
     return np.array(X_vectors)
 
