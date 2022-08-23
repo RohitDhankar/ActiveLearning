35edeac569883ce1e2f5119f1b6dd12234541353
ZhouEEEEEE <zhoueric0603@gmail.com>
2022-08-23 09:08:20 -0400

    Modify diversity

--- FOIL/strategies/diversity.py
+++ FOIL/strategies/diversity.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 
-from strategies.utils import *
+from FOIL.strategies.utils import *
 
 
 def diversity_sampling_strategy_global(classifier, X, n_instances=1):
@@ -11,6 +11,8 @@ def diversity_sampling_strategy_global(classifier, X, n_instances=1):
     centroids = np.random.choice(range(len(X)), size=n_instances, replace=False)
     # print("centroids：", centroids)
     changed, newCentroids = Update_cen(X, centroids, n_instances)
+    print("length of centriod of first update_cen", len(newCentroids))
+    print("Current cen", newCentroids)
     while changed > 0:
         changed, newCentroids = Update_cen(X, newCentroids, n_instances)
 
@@ -25,6 +27,7 @@ def diversity_sampling_strategy_global(classifier, X, n_instances=1):
         cluster[j].append(i)
 
     # return centroids, cluster
+    print("Diversity Success")
     return centroids, np.array(X)[centroids]
 
 
@@ -47,12 +50,17 @@ def diversity_sampling_strategy_local(classifier, X, n_instances=1):
 
 # Helper
 def Distance(dataSet, centroids, k) -> np.array:
+    print("current length of centroids", len(centroids))
+    print("Current cen", centroids)
     dis = []
-    for sample in dataSet:
+    for idex, sample in enumerate(dataSet):
         # if sample not in centroids:
         cent_sim = []
         for cent in centroids:
-            cent_sim.append(similarity_sample(sample, dataSet[cent]))
+            if idex != cent:
+                cent_sim.append(similarity_sample(sample, dataSet[cent]))
+            else:
+                cent_sim.append(9999)
         dis.append(np.array(cent_sim))
     dis = np.array(dis)
     return dis
@@ -70,7 +78,7 @@ def Update_cen(dataSet, centroids, k):
         cluster.append([])
     for i, j in enumerate(maxIndex):
         cluster[j].append(i)
-    # print("cluster: ", cluster)
+    print("cluster: ", cluster)
     # Find centroid for each cluster
     newCentroids = []
     for i in range(k):
@@ -81,6 +89,7 @@ def Update_cen(dataSet, centroids, k):
                 sam_sum += similarity_sample(dataSet[sample], dataSet[other_sam])
             sam_sum_lst.append(sam_sum)
         index_lst = [cluster[i][j] for j, value in enumerate(sam_sum_lst) if value == max(sam_sum_lst)]
+        print("index_lst", index_lst)
         if index_lst:
             max_index = index_lst[0]
         else:
